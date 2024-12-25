fa634c24442568648c4a16326247e26a4c9b780e
envi <alexgolubev1404@gmail.com>
2024-12-25 16:26:33 +0700

    Models update

--- web/app.py
+++ web/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
-from models import User, db, Product
+from models import Users, db, Products
 import os
 
 WEBHOOK_LISTEN = "0.0.0.0"
